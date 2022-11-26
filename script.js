c02f964216580f6c68fefbb63d5c8d75c0295eeb
Pulsarter <Pulsarter@gmail.com>
2022-11-26 20:06:31 +0100

    Added NavLinksHoverAnimation & ActiveLinkStyle

--- script.js
+++ script.js
@@ -1,50 +1,53 @@
-// window.onscroll = function() {myFunction()};
-
-// var navbar = document.getElementById("navbar");
-// var sticky = navbar.offsetTop;
-
-// function myFunction() {
-//   if (window.pageYOffset >= sticky) {
-//     navbar.classList.add("sticky")
-//   } else {
-//     navbar.classList.remove("sticky");
-//   }
-// }
-
 document.addEventListener('DOMContentLoaded', () => {
    
-    // Menu hamburger moves
-    const navIcon4 = document.querySelector("#nav-icon4");
-    navIcon4.addEventListener("click", () => {
-        navIcon4.classList.toggle("open");
+    // Hamburger button animations
+    const mobileMenuIcon = document.getElementById("mobile-menu-icon");
+    mobileMenuIcon.addEventListener("click", () => {
+        mobileMenuIcon.classList.toggle("open");
         console.log("testVanilla");
     });
-
+    
     // Menu slider moves
     const menuHamburger = document.querySelector(".menu-hamburger");
     const navLinks = document.querySelector(".nav-links");
     menuHamburger.addEventListener("click", () => {
         navLinks.classList.toggle("mobile-menu");
-        console.log("hello");
+        console.log("hello4");
     });
-
+    
     // Button to top
-    let mybutton = document.getElementById("myBtn");
+    const mybutton = document.getElementById("goToTopButton");
 
     // When the user scrolls down 20px from the top of the document, show the button
     window.onscroll = function() {scrollFunction()};
 
     function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        mybutton.style.display = "block";
-    } else {
-        mybutton.style.display = "none";
+        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+            mybutton.style.display = "block";
+        } else {
+            mybutton.style.display = "none";
+        }
     }
-    }
+
+    // Navbar menuLinks animations
+    // https://bobbyhadz.com/blog/javascript-add-event-listener-to-all-elements-with-class
+    const menuLinks = document.querySelectorAll(".link");
+    menuLinks.forEach(link => {
+        link.addEventListener('click',() =>{
+            menuLinks.forEach(link => {
+                link.classList.remove("active");
+            });
+            link.classList.add("active");
+            console.log("link clicked");
+            mobileMenuIcon.classList.remove("open");
+            navLinks.classList.remove("mobile-menu");
+        })   
+    });    
+
 }, false);
 
 // When the user clicks on the button, scroll to the top of the document
-function topFunction() {
+function goToTopFunction() {
     document.body.scrollTop = 0; // For Safari
     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
